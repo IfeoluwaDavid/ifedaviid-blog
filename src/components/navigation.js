14b8fb7c1d7947380a6bde76395689dac4a7b9dd
IfeoluwaDavid <ifeoluwaadese@ymail.com>
2021-11-14 22:40:47 -0500

    fix flickering icon-size on page load

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,12 +6,15 @@ import ifedaviidLogo from '../images/ifedaviid-favicon.png'
 import * as styles from './navigation.module.css'
 import styled from 'styled-components'
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
 const MenuIcon = styled(FontAwesomeIcon)`
   color: #fff;
   margin-left: 0.5rem;
   transform: scale(2);
   align-self: center;
-  display: none;
+
   @media (max-width: 768px) {
     display: flex;
   }
@@ -19,10 +22,12 @@ const MenuIcon = styled(FontAwesomeIcon)`
 
 const Navigation = () => (
   <nav role="navigation" className={styles.container} aria-label="Main">
-    <MenuIcon icon={faBars} />
+    <style src="@fortawesome/fontawesome-svg-core/styles.css">
+      <MenuIcon icon={faBars} />
+    </style>
     <Link to="/" className={styles.logoLink}>
       <span />
-      <img className={styles.logo} alt="CheckmarkIcon" src={ifedaviidLogo} />
+      <img className={styles.logo} alt="ifedaviid-logo" src={ifedaviidLogo} />
       <p className={styles.blogTitle}>IFEDAVIID</p>
     </Link>
     <ul className={styles.navigation}>
