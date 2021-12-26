70def1c8dd9d47414e2d9c9a33a166397660047c
IfeoluwaDavid <ifeoluwaadese@ymail.com>
2021-12-26 05:42:30 -0500

    Switched class components to functional components

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,19 +6,18 @@ import Seo from './seo'
 import Navigation from './navigation'
 import Footer from './footer'
 import * as styles from './layout.module.css'
-class Template extends React.Component {
-  render() {
-    const { children } = this.props
 
-    return (
-      <div>
-        <Seo />
-        <Navigation />
-        <main className={styles.main}>{children}</main>
-        <Footer />
-      </div>
-    )
-  }
+import React from 'react'
+
+export const Template = ({ children }) => {
+  return (
+    <div>
+      <Seo />
+      <Navigation />
+      <main className={styles.main}>{children}</main>
+      <Footer />
+    </div>
+  )
 }
 
 export default Template
